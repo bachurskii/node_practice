e9ac2b1ebcb7950f61644bcf7623eb192a742dc1
Georgen1 <123319264+bachurskii@users.noreply.github.com>
2023-09-24 15:04:32 +0300

    node_practice

---
+++ contact.js
@@ -0,0 +1,57 @@
+"use strict";
+
+import fs from "fs/promises";
+import path from "path";
+
+const contactPath = path.resolve("db", "contacts.json");
+
+export async function listContact() {
+  try {
+    const data = await fs.readFile(contactPath, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getContactsById(contactId) {
+  try {
+    const contacts = await listContact();
+    return contacts.find((contact) => contact.id === contactId) || null;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function removeContact(contactId) {
+  try {
+    const contacts = await listContact();
+    const contactIndex = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
+    if (contactIndex === -1) {
+      return null;
+    }
+    const removeContact = contacts.slice(contactIndex, 1)[0];
+    await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+    return removeContact;
+  } catch (error) {
+    throw error;
+  }
+}
+export async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContact();
+    const newContact = {
+      id: Date.now().toString(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ index.js
@@ -0,0 +1,67 @@
+"use strict";
+
+import {
+  listContact,
+  getContactsById,
+  removeContact,
+  addContact,
+} from "./contact.js";
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const { action, id, name, email, phone } = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContact().then((contacts) => {
+        console.log("Список контактов");
+        console.log(contacts);
+      });
+      break;
+
+    case "get":
+      getContactsById(id).then((contacts) => {
+        if (contacts) {
+          console.log(`Контакт с ID:${id}`);
+          console.log(contacts);
+        } else {
+          console.error("Нету такого контакта");
+        }
+      });
+      break;
+
+    case "add":
+      addContact(name, email, phone).then((newContact) => {
+        console.log("Добавлен новый контакт");
+        console.log(newContact);
+      });
+      break;
+
+    case "remove":
+      removeContact(id).then((removedContact) => {
+        if (removedContact) {
+          console.log(`Удаленый контакт с ID:${id}`);
+          console.log(removedContact);
+        } else {
+          console.error("Нету такого контакта");
+        }
+      });
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction({ action, id, name, email, phone });
